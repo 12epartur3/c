55a6ba954a758f19c10c521c13a640972e25a7b0
12epartur3 <1012698305@qq.com>
2023-03-06 14:58:49 +0800

    Update forwarding.cpp

--- cpp/forwarding.cpp
+++ cpp/forwarding.cpp
@@ -6,5 +6,6 @@
 // 右值引用可以绑定到那些非const左值引用不等绑定的右值，这里说的其实就是临时值/将亡值
 
 // 完美转发
-// When you combine rvalue references with function templates you get an interesting interaction: if the type of a function parameter is an rvalue reference to a template type parameter then the type parameter is deduce to be an lvalue reference if an lvalue is passed, and a plain type otherwise
-// 当右值引用和模板
+// When you combine rvalue references with function templates you get an interesting interaction: 
+// if the type of a function parameter is an rvalue reference to a template type parameter then the type parameter is deduce to be an lvalue reference if an lvalue is passed, and a plain type otherwise
+// 当右值引用和模板函数放在一起的时候，会有一些奇怪的现象：，那么该参数的类型将退化成&x，
