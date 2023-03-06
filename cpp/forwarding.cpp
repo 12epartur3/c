be93182c18823d61893a9f100ef43d362c50a85b
12epartur3 <1012698305@qq.com>
2023-03-06 15:01:09 +0800

    Update forwarding.cpp

--- cpp/forwarding.cpp
+++ cpp/forwarding.cpp
@@ -8,4 +8,4 @@
 // 完美转发
 // When you combine rvalue references with function templates you get an interesting interaction: 
 // if the type of a function parameter is an rvalue reference to a template type parameter then the type parameter is deduce to be an lvalue reference if an lvalue is passed, and a plain type otherwise
-// 当右值引用和模板函数放在一起的时候，会有一些奇怪的现象：，那么该参数的类型将退化成&x，
+// 当右值引用和模板函数放在一起的时候，会有一些奇怪的现象：如果函数的参数类型是模板参数的右值引用&&T，如果传一个左值进来给这个函数，那么这个函数参数将退化成左值引用，如果是右值，那么这个参数就变成一个普通类型。
