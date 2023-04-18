615bae24b4d34eb5f5dc50ae7411ef0ed35cb41b
12epartur3 <1012698305@qq.com>
2023-04-18 16:51:10 +0800

    Update class_virtual.cpp

--- cpp/class_virtual.cpp
+++ cpp/class_virtual.cpp
@@ -9,15 +9,15 @@ class B : public virtual A {
 
 class C : public virtual A {
 };
-//到这里，B和C中分别有一个vptr，实例化后指向自己对应的虚基类A
+//到这里，B和C中分别有一个vptr，这个vptr指向 virtual base calss table，真正的virtual base class指针放在该表格中
 
 class D : public B C {
 };
-//这里就是菱形继承，D由B和C构成，B和C中两个vptr指向同一个虚基类A，实现对虚基类的复用
-//以上是Microsoft的实现
+// 这里就是菱形继承，D由B和C构成，B和C中两个vptr指向两张表，表中第一项指向同一个虚基类A，实现对虚基类的复用
+// 以上是Microsoft的实现
 
 // 深度探索c++对象模型，第三章page157
-// Bjarne是通过将虚基类的偏移放在虚函数表中实现的。
+// Bjarne是通过将虚基类的偏移放在虚函数表中实现的，这里放的是虚基类相对于该派生类起始地址的offset。
 // B放有一个vptr指向虚函数表，虚函数表的正向偏移*vptr[x]，x >= 0，存放的是虚函数的地址，负向偏移，x<0，存放的是虚基类相对于B起始地址的偏移。
 // C同样也是这样实现。
 
@@ -38,4 +38,4 @@ Point3d origin, *pt = &origin;
 origin.x = 0.0;
 pt->x = 0.0;
 
-当origin是一个派生类，并且继承结构中有一个虚基类，并且x是从该虚基类继承过来的member时，会有重大的差异
+// 当origin是一个派生类，并且继承结构中有一个虚基类，并且x是从该虚基类继承过来的member时，会有重大的差异
